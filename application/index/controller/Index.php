87186d207858954476ab97eec04c6bd705f59b6b
xingyuewu <qi_zhengning@163.com>
2018-01-18 09:27:55 +0800

    123

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,13 @@
 <?php
 namespace app\index\controller;
+use php\api_demo\SmsDemo;
 
 class Index
 {
     public function index()
     {
-        echo 111;
+        $sms = new SmsDemo();
+        $response = SmsDemo::sendSms();
+        dump($response);exit;
     }
 }
